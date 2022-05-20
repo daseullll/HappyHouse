10f6e36266d6156589a4263ee5821be58e6c0412
이정민 <jmlee9707@naver.com>
2022-05-20 10:10:41 +0900

    vuetify hate..

---
+++ frontend/frontend/src/views/UserView.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="view">userView</div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="scss">
+.view {
+  background-color: $KAKAO-YELLOW;
+}
+</style>
