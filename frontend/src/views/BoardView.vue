9174f9690cf11295cdb7ce6be60072281e6c322f
이정민 <jmlee9707@naver.com>
2022-05-20 10:09:52 +0900

    vuetify hate..

--- frontend/src/views/BoardView.vue
+++
@@ -1,6 +0,0 @@
-<template>
-  <div class="container">
-    <h2 class="text-center">뷰뷰뷰뷰뷰뷰</h2>
-    <router-view></router-view>
-  </div>
-</template>
--- frontend/src/views/NoticeView.vue
+++
@@ -1,6 +0,0 @@
-<template>
-  <div class="container">
-    <h2 class="text-center">community</h2>
-    <router-view></router-view>
-  </div>
-</template>
