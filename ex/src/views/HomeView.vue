560d6b285ff3a45fdd86aad248de49852666cb1d
안다슬 <dew4325@naver.com>
2022-05-24 04:13:48 +0900

    ㅜㅜ

--- ex/src/views/HomeView.vue
+++ ex/src/views/HomeView.vue
@@ -1,5 +1,4 @@
 <template>
-<<<<<<< HEAD
   <div id="fullpage main banner">
     <!--section 1 start-->
     <section class="active page-container num1">
@@ -92,31 +91,11 @@
 export default {};
 </script>
 
-=======
-  <div class="main-banner">
-    <main-content :msg="msg"></main-content>
-  </div>
-</template>
-
-<script>
-import MainContent from "@/components/MainContent.vue";
-export default {
-  name: "HomeView",
-  components: { MainContent },
-  data() {
-    return {
-      msg: "해피하우스",
-    };
-  },
-};
-</script>
->>>>>>> origin/hi
 <style lang="scss">
 .main-banner {
   height: 540px;
   background-color: $KAKAO_YELLOW;
 }
-<<<<<<< HEAD
 
 .page-container {
   // position: absolute;
@@ -208,6 +187,4 @@ export default {
   margin: 60px 0 60px 0;
   padding: 10px 60px;
 }
-=======
->>>>>>> origin/hi
 </style>
