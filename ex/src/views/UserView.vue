c50c4bf5e3d6428e40590031074eb54a540e949d
이정민 <jmlee9707@naver.com>
2022-05-26 01:11:13 +0900

    aaa
    Former-commit-id: b457b99ff9addc82da5006a0e94c1a8c26f0980e

--- ex/src/views/UserView.vue
+++ ex/src/views/UserView.vue
@@ -2,7 +2,7 @@
   <div class="container outer">
     <div class="title-container">
       <span
-        ><img class="titleIcon" src="@/assets/images/emoji/BlossomEmoji.png" />
+        ><img class="titleIcon" src="@/assets/images/small-hap.png" />
       </span>
       <span class="underline title fs-48 notoBold">마이페이지</span>
     </div>
