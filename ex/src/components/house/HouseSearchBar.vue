19393952eb666c1f7afe7c0e7964e818dbdd2271
안다슬 <dew4325@naver.com>
2022-05-27 03:54:21 +0900

    marker plz
    Former-commit-id: b895eb084082b363b657548a6fdaaae54bb9ac95

--- ex/src/components/house/HouseSearchBar.vue
+++ ex/src/components/house/HouseSearchBar.vue
@@ -188,13 +188,13 @@ export default {
         fragment.appendChild(itemEl);
       }
 
-      for (var idx = 0; idx < 10; idx++) {
+      for (var idx = 0; idx < this.schools.length; idx++) {
         //console.log("Z", this.schools[idx]);
         let placePosition = new kakao.maps.LatLng(
           this.schools[idx].lat,
           this.schools[idx].lng
         );
-        let marker = this.addMarker(placePosition, idx);
+        let marker = this.addMarker2(placePosition, idx);
         let itemEl = this.getListItem(idx, this.schools[idx]); // 검색 결과 항목 Element를 생성합니다
         // 검색된 장소 위치를 기준으로 지도 범위를 재설정하기위해
         // LatLngBounds 객체에 좌표를 추가합니다
@@ -213,7 +213,7 @@ export default {
       //마커 생성하고 지도 위에 마커를 표시
       let imageSrc =
           "https://t1.daumcdn.net/localimg/localimages/07/mapapidoc/marker_number_blue.png", // 마커 이미지 url, 스프라이트 이미지를 씁니다
-        imageSize = new kakao.maps.Size(36, 37), // 마커 이미지의 크기
+        imageSize = new kakao.maps.Size(24, 35), // 마커 이미지의 크기
         imgOptions = {
           spriteSize: new kakao.maps.Size(36, 691), // 스프라이트 이미지의 크기
           spriteOrigin: new kakao.maps.Point(0, idx * 46 + 10), // 스프라이트 이미지 중 사용할 영역의 좌상단 좌표
@@ -233,6 +233,31 @@ export default {
       this.markers.push(marker); // 배열에 생성된 마커를 추가합니다
       return marker;
     },
+    addMarker2(position) {
+      //마커 생성하고 지도 위에 마커를 표시
+      let imageSrc =
+          "https://t1.daumcdn.net/localimg/localimages/07/mapapidoc/markerStar.png", // 마커 이미지 url, 스프라이트 이미지를 씁니다
+        imageSize = new kakao.maps.Size(24, 35), // 마커 이미지의 크기
+        // imgOptions = {
+        //   spriteSize: new kakao.maps.Size(36, 691), // 스프라이트 이미지의 크기
+        //   spriteOrigin: new kakao.maps.Point(0, idx * 46 + 10), // 스프라이트 이미지 중 사용할 영역의 좌상단 좌표
+        //   offset: new kakao.maps.Point(13, 37), // 마커 좌표에 일치시킬 이미지 내에서의 좌표
+        // },
+        markerImage = new kakao.maps.MarkerImage(
+          imageSrc,
+          imageSize
+          //imgOptions
+        ),
+        marker = new kakao.maps.Marker({
+          position: position, // 마커의 위치
+          image: markerImage,
+        });
+      marker.setMap(this.map); // 지도 위에 마커를 표출합니다
+
+      this.markers.push(marker); // 배열에 생성된 마커를 추가합니다
+      return marker;
+    },
+
     removeMarker() {
       //지도 위에 표시되고 있는 모든 마커 제거
       for (var i = 0; i < this.markers.length; i++) {
